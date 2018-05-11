cc28c8ea7ddc10c61121eb2be1fd31f67a981808
Andrea Dall'Alba <HGS-Andry@users.noreply.github.com>
2018-05-12 00:52:06 +0200

    addition of operator to animate lamps and cameras
    -Add operator animate_all to handle the animations
    -modified create_cameras to create multiple cameras by not deleting existing one
    -replaced all list counting in if statement to chek if a list is empty with the proper way to do it
    -create operator to render and to create the export file

--- SyntheticRTI-Plugin/SyntheticRTI.py
+++ SyntheticRTI-Plugin/SyntheticRTI.py
@@ -1,7 +1,6 @@
 import bpy
 from mathutils import Vector
 
-
 ###Global var###
 
 srti_lamp_list = []
@@ -10,6 +9,7 @@ srti_main_parent = []
 
 ###Various function###
 
+####TMP
 def printdebug():
     print("Lamp:")
     print(srti_lamp_list)
@@ -20,8 +20,12 @@ def printdebug():
 
 def populate(context):
     """populate global variables from the main parent name when loading a file"""
+    global srti_main_parent 
+    global srti_lamp_list
+    global srti_camera_list
+
     parent_name = context.scene.srti_main_parent_prop
-    if parent_name != "":
+    if parent_name:
         print(parent_name)
         main_parent = context.scene.objects[parent_name]
         srti_main_parent.append(main_parent)
@@ -40,7 +44,9 @@ def populate(context):
 def create_main(scene):
     """Create the main object if not already present"""
     #Aggiungo un empty a cui collegare tutte le luci per poter modificarle come gruppo
-    if len(srti_main_parent) == 0:
+    global srti_main_parent 
+
+    if not srti_main_parent:
         main_parent = bpy.data.objects.new(name = "main_parent", object_data = None)
         scene.objects.link(main_parent)
         main_parent.empty_draw_type = 'SPHERE'
@@ -49,10 +55,11 @@ def create_main(scene):
         scene.srti_main_parent_prop = main_parent.name
 
 def delete_main(scene):
-    """Delete main parent""" 
+    """Delete main parent"""
+    global srti_main_parent 
+
     bpy.ops.object.select_all(action='DESELECT')
-    global srti_main_parent
-    if len(srti_main_parent) > 0:
+    if srti_main_parent:
         srti_main_parent[0].select = True #delete the main parent (need revision)
     bpy.ops.object.delete()
     srti_main_parent = []
@@ -84,20 +91,19 @@ class create_lamps(bpy.types.Operator):
         digit_luci = len(str(n_luci)) #ricavo il numero di caratteri massimo per scrivere i numeri delle lampade
         print(n_luci)
         
+        ##Uso una luce standard, TODO aggiungere un oggetto
         lamp_data = bpy.data.lamps.new(name="Luce_progetto", type='SUN') # Create new lamp datablock. It s going to be created outside
+
         
-        curr_scene.frame_start = 1
-        curr_scene.frame_end = n_luci
-        
-        for i in range(1, n_luci + 1): #passo tutte le luci 
-            valori_riga = righe[i].split() #divido i valori
+        for lamp in range(1, n_luci + 1): #passo tutte le luci 
+            valori_riga = righe[lamp].split() #divido i valori
             lmp_x = float(valori_riga[1])
             lmp_y = float(valori_riga[2])
             lmp_z = float(valori_riga[3])
             direction = Vector((lmp_x, lmp_y, lmp_z))
 
-            print(i , 'x=', lmp_x, 'y=', lmp_y, 'z=', lmp_z) #stampo i valori per riga
-            lamp_object = bpy.data.objects.new(name="Lampada_{0:0>{dig}}".format(i, dig = digit_luci), object_data=lamp_data) # Create new object with our lamp datablock
+            print(lamp , 'x=', lmp_x, 'y=', lmp_y, 'z=', lmp_z) #stampo i valori per riga
+            lamp_object = bpy.data.objects.new(name="Lampada_{0:0>{dig}}".format(lamp, dig = digit_luci), object_data=lamp_data) # Create new object with our lamp datablock
             curr_scene.objects.link(lamp_object) # Link lamp object to the scene so it'll appear in this scene
             lamp_object.parent = main_parent
             lamp_object.location = (lmp_x, lmp_y, lmp_z) # Place lamp to a specified location
@@ -105,16 +111,7 @@ class create_lamps(bpy.types.Operator):
             lamp_object.rotation_mode = 'QUATERNION'
             lamp_object.rotation_quaternion = direction.to_track_quat('Z','Y')
             
-            #animo le luci
-            #bpy.ops.anim.change_frame(frame = i-1)
-            #nascondo le lampade nel frame precedente e nel sucessivo
-            lamp_object.hide_render = True
             lamp_object.keyframe_insert(data_path = 'hide_render', frame = i-1)
-            lamp_object.keyframe_insert(data_path = 'hide_render', frame = i+1)
-            #rendo visibile la lampada solo nel suo frame
-            lamp_object.hide_render = False
-            lamp_object.keyframe_insert(data_path = 'hide_render', frame = i)
-            
             srti_lamp_list.append(lamp_object)
 
         return{'FINISHED'}
@@ -130,19 +127,20 @@ class delete_lamps(bpy.types.Operator):
 
         bpy.ops.object.select_all(action='DESELECT')
 
-        if len(srti_lamp_list) > 0:
+        if srti_lamp_list:
             for obj in srti_lamp_list:
                 obj.select = True
     
         bpy.ops.object.delete()
 
-        if len(srti_camera_list) == 0: #delete the main object if no cameras
+        if not srti_camera_list: #delete the main object if no cameras
             delete_main(context.scene)
 
         srti_lamp_list = [] #delete information in global variable
 
         return{'FINISHED'}
 
+########CAMERA########
 class create_cameras(bpy.types.Operator):
     """Create cameras"""
     bl_idname = "srti.create_cameras"
@@ -150,7 +148,7 @@ class create_cameras(bpy.types.Operator):
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        bpy.ops.srti.delete_cameras() #delete exsisting cameras
+        #bpy.ops.srti.delete_cameras() #delete exsisting cameras
 
         curr_scene = context.scene
 
@@ -169,8 +167,6 @@ class create_cameras(bpy.types.Operator):
 
         return{'FINISHED'}
 
-
-
 class delete_cameras(bpy.types.Operator):
     """Delete all cameras"""
     bl_idname = "srti.delete_cameras"
@@ -181,20 +177,98 @@ class delete_cameras(bpy.types.Operator):
         global srti_camera_list
         bpy.ops.object.select_all(action='DESELECT')
         
-        if len(srti_camera_list) > 0:
+        if srti_camera_list:
             for obj in srti_camera_list:
                 obj.select = True
     
         bpy.ops.object.delete()
 
-        if len(srti_lamp_list) == 0: #delete the main object if no lamps
+        if not srti_lamp_list: #delete the main object if no lamps
              delete_main(context.scene)
         
         srti_camera_list = []
               
         return{'FINISHED'}
 
+####ANIMATION########
+class animate_all(bpy.types.Operator):
+    """Animate all lights, cameras and parameter"""
+    bl_idname = "srti.animate_all"
+    bl_label = "Animate all"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
 
+        #TODO gestire le proprietà
+        global srti_camera_list
+        global srti_lamp_list
+        global srti_main_parent
+
+        index_light = 0
+        index_cam = 0
+        index_prop = 0
+
+        tot_light = len(srti_lamp_list)
+        tot_cam = len(srti_camera_list)
+        
+        curr_scene = context.scene
+        
+        curr_scene.frame_start = 1
+        curr_scene.frame_end = tot_cam * tot_light
+
+        #Delete animations
+        for cam in srti_camera_list:
+            cam.animation_data_clear()
+
+        for lamp in srti_lamp_list:
+            lamp.animation_data_clear()
+
+        #delete markers
+        curr_scene.timeline_markers.clear()
+        
+        #TODO loop property
+
+        for cam in srti_camera_list: #loop every camera
+            mark = curr_scene.timeline_markers.new(cam.name, index_prop + index_cam * tot_light + 1) # create a marker
+            mark.camera = cam
+            for lamp in srti_lamp_list: #loop every lamp
+                #animate lamps
+                curr_frame = (index_prop * tot_cam * tot_light) + (index_cam * tot_light) + index_light + 1
+                #hide lamp on theprevious and next frame
+                lamp.hide_render = True
+                lamp.keyframe_insert(data_path = 'hide_render', frame = curr_frame-1)
+                lamp.keyframe_insert(data_path = 'hide_render', frame = curr_frame+1)
+                #rendo visibile la lampada solo nel suo frame
+                lamp.hide_render = False
+                lamp.keyframe_insert(data_path = 'hide_render', frame = curr_frame)
+
+                index_light += 1
+
+            index_cam += 1
+            index_light = 0
+            
+        return{'FINISHED'}
+
+#####RENDERING######
+class render_images(bpy.types.Operator):
+    """Render all images"""
+    bl_idname = "srti.render_images"
+    bl_label = "Render"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
+        #TODO è poi così necessario?
+        return{'FINISHED'}
+
+class create_export_file(bpy.types.Operator):
+    """Create a file with all the iamges name and parameters"""
+    bl_idname = "srti.create_file"
+    bl_label = "Create file"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute(self, context):
+        #TODO
+        return{'FINISHED'}
     
 ##############
 #####GUI######
@@ -219,6 +293,10 @@ class SyntheticRTIPanel(bpy.types.Panel):
         layout.label("main = %s" % (str(len(srti_main_parent))))
         layout.label("lamps = %s" % (str(len(srti_lamp_list))))
         layout.label("cameras = %s" % (str(len(srti_camera_list))))
+        layout.label("frame totali = %s" % (str(len(srti_camera_list) * len(srti_lamp_list))))
+        layout.operator("srti.animate_all")
+        layout.operator("srti.render_images")
+        layout.operator("srti.create_file")
 
 def register():
     ##Scene data definition
@@ -234,6 +312,9 @@ def register():
     bpy.utils.register_class(delete_cameras)
     bpy.utils.register_class(create_lamps)
     bpy.utils.register_class(create_cameras)
+    bpy.utils.register_class(create_export_file)
+    bpy.utils.register_class(render_images)
+    bpy.utils.register_class(animate_all)
     bpy.utils.register_class(SyntheticRTIPanel)
     populate(bpy.context)
 
@@ -242,7 +323,10 @@ def unregister():
     bpy.utils.unregister_class(create_cameras)
     bpy.utils.unregister_class(create_lamps)
     bpy.utils.unregister_class(delete_cameras)
-    bpy.utils.unregister_class(delete_lamps)    
+    bpy.utils.unregister_class(delete_lamps)
+    bpy.utils.unregister_class(create_export_file)
+    bpy.utils.unregister_class(render_images)
+    bpy.utils.unregister_class(animate_all)
     ##Delete of custom rna data
     del bpy.types.Scene.srti_lightsFilePath
     del bpy.types.Scene.srti_main_parent_prop
